a8e486714a65fa827ae392fa79d75f01fb1ea3f1
Thomas Cilano <Tom@Thomass-MacBook-Pro.local>
2016-10-04 14:23:28 -0400

    workinghwANDsql

---
+++ src/main/java/sql/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE lineitem
+(
+    lin_description VARCHAR(155),
+    lin_category VARCHAR(155),
+    lin_budgetedamount NUMERIC,
+    lin_actualamount NUMERIC,
+    lin_id INTEGER PRIMARY KEY NOT NULL
+);
---
+++ src/main/java/sql/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Groceries', 'Food', 400, 350, 1);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Dining out', 'Food', 100, 200, 3);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Internet', 'Services', 100, 100, 4);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Gas', 'Transportation', 100, 130, 2);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Cell Phone', 'Services', 100, 100, 5);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Property Tax', 'Household', 200, 200, 6);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Power', 'Services', 120, 130, 7);
+INSERT INTO budget.lineitem (lin_description, lin_category, lin_budgetedamount, lin_actualamount, lin_id) VALUES ('Beer', 'Food', 100, 200, 8);
